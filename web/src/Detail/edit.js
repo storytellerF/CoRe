e2d3b6881d4af3c18282796cc401583a41399b4c
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-08-15 18:51:03 +0800

    extract method

--- web/src/Detail/edit.js
+++ web/src/Detail/edit.js
@@ -5,107 +5,31 @@ import iziToast from "izitoast";
 import storage from "store"
 import { ConstantsContext } from "../Context/ConstantsContext";
 import mermaid from "mermaid";
-function resolveCurrentLine(lines, cursorPosition) {
+import handleTab from "./handle-tab";
 
-    let currentLineNumber = 0
-    let currentLineEnd = 0
-    for (; currentLineNumber < lines.length; currentLineNumber++) {
-        currentLineEnd += lines[currentLineNumber].length + 1
-        if (currentLineEnd > cursorPosition) {
-            break
-        }
+function getFormData(title, content, id) {
+    if (content.length === 0) {
+        iziToast.show({
+            color: "red",
+            title: "content is empty",
+            position: "center"
+        })
+        return
     }
-    const currentLineLength = lines[currentLineNumber].length
-    const currentLineStart = currentLineEnd - currentLineLength - 1
-
-    return {
-        currentLineNumber,
-        "currentLineContent": lines[currentLineNumber],
-        currentLineLength,
-        currentLineStart,
-        currentLineEnd,
+    if (title.length === 0) {
+        iziToast.show({
+            color: "red",
+            title: "title is empty",
+            position: "center"
+        })
+        return
     }
-}
-
-function shiftTabSingleLine(content, start) {
-    const lines = content.split("\n")
-
-    const lineGroup = resolveCurrentLine(lines, start)
-    const currentContent = lineGroup.currentLineContent;
-
-    var { newLine, spaceCount } = shiftTab(currentContent);
-
-    lines[lineGroup.currentLineNumber] = newLine
-    let result = {
-        value: lines.join("\n"),
-        start: start - spaceCount,
-        end: start - spaceCount
-    };
-
-    return result;
-}
-
-function shiftTab(currentContent) {
-    let spaceCount = 0;
-    const hasTab = currentContent.startsWith("\t");
-    if (hasTab) {
-        spaceCount = 1;
-    } else {
-        for (let i = 0; i < currentContent.length && i < 4; i++) {
-            if (currentContent[i] === ' ') spaceCount++;
-            else break;
-        }
-    }
-    const newLine = currentContent.substring(spaceCount);
-    return { newLine, spaceCount };
-}
-
-function shiftTabMultiLine(content, start, end) {
-    const lines = content.split("\n")
-
-    const startGroup = resolveCurrentLine(lines, start)
-    const endGroup = resolveCurrentLine(lines, end)
-    console.log(startGroup)
-    console.log(endGroup)
-    let startOffset
-    let endOffset = 0
-    if (startGroup.currentLineNumber !== endGroup.currentLineNumber) {
-        for (let i = startGroup.currentLineNumber; i <= endGroup.currentLineNumber; i++) {
-            var { newLine, spaceCount } = shiftTab(lines[i])
-            lines[i] = newLine
-            if (i == startGroup.currentLineNumber) {
-                startOffset = spaceCount
-            }
-            endOffset += spaceCount
-        }
-    }
-    return {
-        value: lines.join("\n"),
-        start: start - startOffset,
-        end: end - endOffset
-    }
-}
-
-function handleTab(start, end, tempValue, inputShift) {
-    let result
-    if (start === end) {
-        if (!inputShift) {
-            const before = tempValue.substring(0, start)
-            const after = tempValue.substring(end)
-
-            result = {
-                value: before + "\t" + after,
-                start: start + 1,
-                end: start + 1
-            }
-        } else if (start === end) {
-            result = shiftTabSingleLine(tempValue, start)
-        }
-    } else {
-        if (inputShift)
-            result = shiftTabMultiLine(tempValue, start, end)
-    }
-    return result
+    const formData = new FormData()
+    formData.append("title", title)
+    formData.append("codeContent", content)
+    if (id != null)
+        formData.append("id", id)
+    return formData
 }
 
 function Editor({ marked }) {
@@ -200,27 +124,8 @@ function Editor({ marked }) {
     }, [nextSelection])
     const handleSubmit = (event) => {
         event.preventDefault()
-        if (content.length === 0) {
-            iziToast.show({
-                color: "red",
-                title: "content is empty",
-                position: "center"
-            })
-            return
-        }
-        if (title.length === 0) {
-            iziToast.show({
-                color: "red",
-                title: "title is empty",
-                position: "center"
-            })
-            return
-        }
-        const formData = new FormData()
-        formData.append("title", title)
-        formData.append("codeContent", content)
-        if (id != null)
-            formData.append("id", id)
+        const formData = getFormData(title, content, id)
+        if (formData == undefined) return
         let postLink
         if (id != null) postLink = "/edit"
         else postLink = "/add"
---
+++ web/src/Detail/handle-tab.js
@@ -0,0 +1,104 @@
+function resolveCurrentLine(lines, cursorPosition) {
+
+    let currentLineNumber = 0
+    let currentLineEnd = 0
+    for (; currentLineNumber < lines.length; currentLineNumber++) {
+        currentLineEnd += lines[currentLineNumber].length + 1
+        if (currentLineEnd > cursorPosition) {
+            break
+        }
+    }
+    const currentLineLength = lines[currentLineNumber].length
+    const currentLineStart = currentLineEnd - currentLineLength - 1
+
+    return {
+        currentLineNumber,
+        "currentLineContent": lines[currentLineNumber],
+        currentLineLength,
+        currentLineStart,
+        currentLineEnd,
+    }
+}
+
+function shiftTabSingleLine(content, start) {
+    const lines = content.split("\n")
+
+    const lineGroup = resolveCurrentLine(lines, start)
+    const currentContent = lineGroup.currentLineContent;
+
+    var { newLine, spaceCount } = shiftTab(currentContent);
+
+    lines[lineGroup.currentLineNumber] = newLine
+    let result = {
+        value: lines.join("\n"),
+        start: start - spaceCount,
+        end: start - spaceCount
+    };
+
+    return result;
+}
+
+function shiftTab(currentContent) {
+    let spaceCount = 0;
+    const hasTab = currentContent.startsWith("\t");
+    if (hasTab) {
+        spaceCount = 1;
+    } else {
+        for (let i = 0; i < currentContent.length && i < 4; i++) {
+            if (currentContent[i] === ' ') spaceCount++;
+            else break;
+        }
+    }
+    const newLine = currentContent.substring(spaceCount);
+    return { newLine, spaceCount };
+}
+
+function shiftTabMultiLine(content, start, end) {
+    const lines = content.split("\n")
+
+    const startGroup = resolveCurrentLine(lines, start)
+    const endGroup = resolveCurrentLine(lines, end)
+    console.log(startGroup)
+    console.log(endGroup)
+    let startOffset
+    let endOffset = 0
+    if (startGroup.currentLineNumber !== endGroup.currentLineNumber) {
+        for (let i = startGroup.currentLineNumber; i <= endGroup.currentLineNumber; i++) {
+            var { newLine, spaceCount } = shiftTab(lines[i])
+            lines[i] = newLine
+            if (i == startGroup.currentLineNumber) {
+                startOffset = spaceCount
+            }
+            endOffset += spaceCount
+        }
+    }
+    return {
+        value: lines.join("\n"),
+        start: start - startOffset,
+        end: end - endOffset
+    }
+}
+
+function handleTab(start, end, tempValue, inputShift) {
+    let result
+    if (start === end) {
+        if (!inputShift) {
+            const before = tempValue.substring(0, start)
+            const after = tempValue.substring(end)
+
+            result = {
+                value: before + "\t" + after,
+                start: start + 1,
+                end: start + 1
+            }
+        } else if (start === end) {
+            result = shiftTabSingleLine(tempValue, start)
+        }
+    } else {
+        if (inputShift)
+            result = shiftTabMultiLine(tempValue, start, end)
+    }
+    return result
+}
+
+export default handleTab;
