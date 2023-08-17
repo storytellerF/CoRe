49bf0f69aa722f323c1395d5740a024d386ef121
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-08-17 12:05:54 +0800

    替换github card

--- web/src/Index/index.js
+++ web/src/Index/index.js
@@ -1,9 +1,30 @@
-function Index() {
-  return (
-    <div className="container mt-4 p-5 bg-light rounded" style={{ marginTop: "20px" }}>
-      <h1>核</h1>
+import { useEffect, useRef, useState } from "react";
 
-      <a href="https://github.com/storytellerF/CoRe"><img src="https://gh-card.dev/repos/storytellerF/CoRe.svg" alt="github card"/></a>
+function Index() {
+  const h1 = useRef()
+  const div = useRef()
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      const w = h1.current.clientWidth
+      let n
+      if (w >= 400) n =  400 
+      else n = w
+      div.current.setAttribute("data-width", n)
+      const t = document.getElementsByClassName("github-card")[0]
+      if (t) {
+        window.githubCard.render(t)
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+  return (
+    <div className="container mt-4 p-5 bg-light rounded" style={{ marginTop: "20px" }} >
+      <h1 ref={h1}>核</h1>
+
+      <div className="github-card" data-github="storytellerF/CoRe" ref={div} data-height="150" data-theme="default"></div>
     </div>
   );
 }
